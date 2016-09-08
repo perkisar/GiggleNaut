3fb7492790fcdcf18e2f5e2e613a4e5d6e06fa86
Sarah Perkins <sarah@vancouverbolt.com>
2016-09-08 09:48:29 -0700

    Initial Commit

---
+++ js/brainText.js
@@ -0,0 +1,36 @@
+﻿(function firstTime() {
+
+    left();
+    right();
+})();
+
+function left() {
+    var left = $('h1[id^="left-"]').hide(),
+    i = 0;
+    (function cycle() {
+
+        left.eq(i).fadeIn(800)
+                  .delay(Math.floor(Math.random() * 3000) + 2000)
+                  .fadeOut(800, cycle);
+
+        i = ++i % left.length;
+
+    })();
+}
+
+function right() {
+    var right = $('h1[id^="right-"]').hide(),
+        i = 0;
+
+    (function cycle() {
+
+        right.eq(i).fadeIn(800)
+                  .delay(Math.floor(Math.random() * 3000) + 2000)
+                  .fadeOut(800, cycle);
+
+        i = ++i % right.length;
+
+    })();
+
+}
+    
